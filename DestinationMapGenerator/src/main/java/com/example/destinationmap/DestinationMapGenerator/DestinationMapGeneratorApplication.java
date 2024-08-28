8f6974bfb86879f4a4dfe9e56e1f568f9af12c1b
siddhisalunke123 <siddhisalunke2002@gmail.com>
2024-08-28 21:52:11 +0530

    Done

---
+++ DestinationMapGenerator/src/main/java/com/example/destinationmap/DestinationMapGenerator/DestinationMapGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.destinationmap.DestinationMapGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationMapGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinationMapGeneratorApplication.class, args);
+	}
+
+}
---
+++ DestinationMapGenerator/src/test/java/com/example/destinationmap/DestinationMapGenerator/DestinationMapGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.destinationmap.DestinationMapGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationMapGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
